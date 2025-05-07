680cc6557a564263d29e169ef01078149a2677eb
Aswajith <aswajith.3747@gmail.com>
2025-05-07 13:43:24 +0530

    changed api endpoint

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,5 +1,5 @@
 class Env {
   // static const String apiUrl = 'https://policonn.rtnayush.run.place/api';
-  static const String apiUrl = 'http://10.0.9.48:8080/api';
+  static const String apiUrl = 'https://policonn.rtnayush.run.place/api';
   // Add other environment variables here as needed
-} 
+}
